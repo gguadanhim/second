5039e259c3c2746304282646fb3f2fb05666c225
gguadanhim <gguadanhim@gmail.com>
2016-05-13 00:19:56 -0300

    ajuste 2

--- Second/First/Second.asmx.cs
+++ Second/First/Second.asmx.cs
@@ -28,13 +28,12 @@ namespace Second
             iControleDados = new ControleDados();
         }
 
-
-        [WebMethod]
+        /*[WebMethod]
         public List<DadosRank> BuscarRanking()
         {
             return iControleDados.getDadosRanking();
         }
-
+        */
         [WebMethod]
         public DadosRetorno contaPartidas()
         {
